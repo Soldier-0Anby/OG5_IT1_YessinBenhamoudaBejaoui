55d48563122feaad36165ad79631c0581f399371
user <user@192.168.34.101>
2026-09-03 10:15:55 +0200

    return statement hinzugefügt

--- Taschenrechner_v0/src/Taschenrechner.java
+++ Taschenrechner_v0/src/Taschenrechner.java
@@ -7,14 +7,17 @@ public class Taschenrechner {
 	}
 	
 	public double sub(double zahl1, double zahl2){
+		return zahl1 - zahl2;
 		// has to be implemented
 	}
 	
 	public double mul(double zahl1, double zahl2){
+		return zahl1 * zahl2;
 		// has to be implemented
 	}
 
 	public double div(double zahl1, double zahl2){
+		return zahl1 / zahl2;
 		// has to be implemented
 	}
 	
